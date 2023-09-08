504faaaf6dce7f94c6c43739777674b07f681f3f
Janusz K. Dlugosz <januszprofession@pm.me>
2023-09-08 13:52:44 -0500

    Start of my first project

---
+++ src/components/AddExpenseForm.js
@@ -0,0 +1,79 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { v4 as uuidv4 } from 'uuid';
+
+const AddExpenseForm = (props) => {
+	const { dispatch } = useContext(AppContext);
+
+	const [name, setName] = useState('');
+	const [type, setType] = useState('');
+	const [cost, setCost] = useState('');
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		const expense = {
+			id: uuidv4(),
+			name,
+			type,
+			cost: parseInt(cost),
+		};
+
+		dispatch({
+			type: 'ADD_EXPENSE',
+			payload: expense,
+		});
+
+		setName('');
+		setType('');
+		setCost('');
+	};
+
+	return (
+		<form onSubmit={onSubmit}>
+			<div class='row'>
+				<div class='col-sm col-lg-4'>
+					<label for='name'>Name</label>
+					<input
+						required='required'
+						type='text'
+						class='form-control'
+						id='name'
+						value={name}
+						onChange={(event) => setName(event.target.value)}
+					/>
+				</div>
+				<div class='col-sm col-lg-4'>
+					<label for='type'>Type</label>
+					<input
+						required='required'
+						type='text'
+						class='form-control'
+						id='type'
+						value={type}
+						onChange={(event) => setCost(event.target.value)}
+					/>
+				</div>
+				<div class='col-sm col-lg-4'>
+					<label for='cost'>Cost</label>
+					<input
+						required='required'
+						type='number'
+						class='form-control'
+						id='cost'
+						value={cost}
+						onChange={(event) => setCost(event.target.value)}
+					/>
+				</div>
+			</div>
+			<div class='row mt-3'>
+				<div class='col-sm'>
+					<button type='submit' class='btn btn-primary'>
+						Save
+					</button>
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default AddExpenseForm;
